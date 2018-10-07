a0466c1f3decf5f3f36dc8a37b61ae9a0f20c7f3
Tuand0051 <tuannxd00551@fpt.edu.vn>
2018-10-08 02:09:47 +0700

    baitap

---
+++ exercise/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace exercise
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_click(object sender, RoutedEventArgs e)
+        {
+            
+            txt_content.Text = btn_content.Text;
+            
+        }
+
+        private void txt_content_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+           
+        }
+    }
+}
